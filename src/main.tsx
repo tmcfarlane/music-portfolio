5ff680e9343ae262b784e5423e7816c9e28843b9
Trent <trentonmcfarlane@gmail.com>
2024-07-01 00:17:29 -0700

    add speedinsights

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import { LightTheme, BaseProvider } from "baseui";
 import { Client as Styletron } from "styletron-engine-atomic";
 import App from "./App";
 import "./index.css";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
 const engine = new Styletron();
+
+injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
