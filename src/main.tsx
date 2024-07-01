494ebc4ea8080a3143b88d1d62db92c2ede4f79b
Trent <trentonmcfarlane@gmail.com>
2024-06-30 23:44:37 -0700

    speedinsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider as StyletronProvider } from 'styletron-react';
-import { LightTheme, BaseProvider } from 'baseui';
-import { Client as Styletron } from 'styletron-engine-atomic';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+import { Client as Styletron } from "styletron-engine-atomic";
+import App from "./App";
+import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const engine = new Styletron();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-   <React.StrictMode>
-     <StyletronProvider value={engine}>
-         <BaseProvider theme={LightTheme}>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
         <App />
-       </BaseProvider>
-     </StyletronProvider>
-  </React.StrictMode>,
+        <SpeedInsights />
+      </BaseProvider>
+    </StyletronProvider>
+  </React.StrictMode>
 );
