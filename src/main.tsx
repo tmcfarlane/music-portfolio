09b0faa89f862730d2fb3096ee211c73ab59ea25
Trent <trentonmcfarlane@gmail.com>
2024-07-01 00:12:57 -0700

    remove speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { LightTheme, BaseProvider } from "baseui";
 import { Client as Styletron } from "styletron-engine-atomic";
 import App from "./App";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const engine = new Styletron();
 
@@ -14,7 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
         <App />
-        <SpeedInsights />
       </BaseProvider>
     </StyletronProvider>
   </React.StrictMode>
